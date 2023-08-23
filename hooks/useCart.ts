d12edd719229ebe0c7e3fd0bdb5e0e02a78a8e79
Samiul Alim <xo.ot2001@gmail.com>
2023-08-23 10:30:09 +0600

    Updated useCart Hook

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -1,16 +1,28 @@
+"use client";
+
 import { useAtom } from "jotai";
+import { useEffect } from "react";
 import { cartAtom } from "@/atoms/cartAtom";
 import { productInterface1 } from "@/interfaces/product";
 
 const useCart = () => {
   const [items, setItems] = useAtom(cartAtom);
 
+  useEffect(() => {
+    const items = localStorage.getItem("cartItems");
+    if (items) {
+      setItems(JSON.parse(items));
+    }
+  }, []);
+
   const addItem = (item: productInterface1) => {
+    localStorage.setItem("cartItems", JSON.stringify([...items, item]));
     setItems([...items, item]);
   };
 
   const deleteItem = (item: productInterface1) => {
     const filteredList = items.filter((el) => el._id !== item._id);
+    localStorage.setItem("cartItems", JSON.stringify(filteredList));
     setItems(filteredList);
   };
 
