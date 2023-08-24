fda8c3c03150f831e70c0dc647731a1a71d0f85c
Samiul Alim <xo.ot2001@gmail.com>
2023-08-24 13:11:00 +0600

    Updated Config and Improved Nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { useAuthContext } from "@/hooks/useAuthContext";
+import { useLogout } from "@/hooks/useLogout";
 
 const NavBar = () => {
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <div className="nav flex justify-between px-10 py-3">
-      <Link href="/" className="text-3xl">
+      <Link href="/" className="text-3xl font-semibold">
         TheShop
       </Link>
       <div className="flex ">
@@ -15,10 +26,31 @@ const NavBar = () => {
           About
         </Link>
       </div>
-      <div className="flex">
-        <Link href="/login" className="px-3 py-2">
-          Login
-        </Link>
+      <div className="flex justify-center">
+        {!user && (
+          <Link
+            href="/login"
+            className="px-3 py-2 hover:bg-blue-500 rounded-lg"
+          >
+            Login
+          </Link>
+        )}
+        {user && (
+          <>
+            <button
+              onClick={handleClick}
+              className="hover:bg-red-500 px-3 py-1 rounded-lg"
+            >
+              Logout
+            </button>
+            <Link
+              href="/dashboard"
+              className="px-3 py-2 hover:bg-blue-500 rounded-lg"
+            >
+              Dashboard
+            </Link>
+          </>
+        )}
         <Link href="/cart" className="px-3 py-2">
           Cart
         </Link>
